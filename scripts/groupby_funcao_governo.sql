6920e0c61c243fb5f14bd71154afe5f58876e63f
Lgcsimoes <Lgcsimoes@gmail.com>
2019-01-20 20:33:45 -0200

    Time series notebook added

--- scripts/groupby_funcao_governo.sql
+++ scripts/groupby_funcao_governo.sql
@@ -1,12 +1,17 @@
 USE db_tce;
 
 SELECT 
-    ds_municipio, ds_funcao_governo, SUM(vl_despesa)
+    YEAR(dt_emissao_despesa) AS year,
+    MONTH(dt_emissao_despesa) AS month,
+    ds_municipio, ds_funcao_governo,
+    SUM(vl_despesa) AS vl_despesa
 FROM
     despesas
 WHERE
     ano_exercicio = 2017 AND
     ds_municipio IN
 		(SELECT name_city FROM ibge
-			WHERE population_2018 > 40000)
-GROUP BY ds_municipio ASC , ds_funcao_governo ASC;
+			WHERE population_2018 > 400000)
+GROUP BY
+	ds_municipio ASC , ds_funcao_governo ASC,
+	MONTH(dt_emissao_despesa), YEAR(dt_emissao_despesa);
